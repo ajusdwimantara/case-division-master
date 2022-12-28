1b9c9eeefd1ae9b9af809f738dba0c851b63bed1
ajusdwimantara <108891016+ajusdwimantara@users.noreply.github.com>
2022-12-28 13:18:04 +0700

    Update ros-env.sh

--- ros-env.sh
+++ ros-env.sh
@@ -1,16 +1,13 @@
 #!//usr/bin/bash
 
 
-# Program untuk Robi, robot lain menyesuaikan
+# Program for Robi, other robots please adjust
 M_IP_ALFA="192.168.10.1" #IP ALFA
 M_IP_ABI="192.168.10.2" #IP ABI
 M_IP_ROBI="192.168.10.34" #IP ROBI
 
-#IP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')" #dari github
-
 # Get the robot's IP
 IP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
-#IP=$M_IP_ROBI #pake sementara, soalnya gabisa baca ip terakhir yg 2 digit
 
 # Try reaching for Alfa
 if ping -c 1 $M_IP_ALFA > /dev/null
@@ -44,12 +41,3 @@ export ROS_MASTER_URI="http://${M_IP}:11311"
 export ROS_IP="${IP}"
 export ROS_HOSTNAME="${ROS_IP}"
 
-if [ "$master" == "ROBI" ] || [ -z "$master" ]
-then
-  echo Launching Main Service
-  roscore
-  #create a new terminal tab
-  #xdotool key  --clearmodifiers --delay 40 "ctrl+shift+t" 
-  #launch mainservice
-  #roslaunch <package_name> <launch_file>
-fi
